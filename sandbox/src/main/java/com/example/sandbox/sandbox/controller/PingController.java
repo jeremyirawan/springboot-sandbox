20bee10ee4771abbf609318c348689ebf91c4c61
Ramadhi Irawan <jeremyrirawan@gmail.com>
2024-02-02 17:34:51 +0000

    Adding new dependency

--- sandbox/src/main/java/com/example/sandbox/sandbox/controller/PingController.java
+++ sandbox/src/main/java/com/example/sandbox/sandbox/controller/PingController.java
@@ -1,12 +1,18 @@
 package com.example.sandbox.sandbox.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController {
+
+    Logger logger = LoggerFactory.getLogger(PingController.class);
+
     @GetMapping("/ping")
     public String ping() {
+        logger.info("Incoming request to /ping endpoint...");
         return "{\"message\": \"pong\"}";
     }
 }
