3f12850f643bd8adf8f18a649bb2467843361b87
Ramadhi Irawan <jeremyrirawan@gmail.com>
2024-02-03 09:34:47 +0000

    changes in SandboxApplication

--- sandbox/src/main/java/com/example/sandbox/sandbox/SandboxApplication.java
+++ sandbox/src/main/java/com/example/sandbox/sandbox/SandboxApplication.java
@@ -3,13 +3,10 @@ package com.example.sandbox.sandbox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class SandboxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SandboxApplication.class, args);
 	}
-
 }
